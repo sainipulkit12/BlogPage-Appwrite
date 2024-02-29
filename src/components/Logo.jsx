cd7c5dbcd247f351097111468440bb5c233371eb
Pulkit saini <51844439+sainipulkit12@users.noreply.github.com>
2024-03-01 02:37:50 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({ width = "10px" }) {
+  return (
+    <div>
+     Blog
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Container, PostForm } from '../components'
+import service from '../appwrite/config'
+import { useParams, useNavigate } from 'react-router-dom'
+
+function EditPost() {
+    const [post, setPosts] = useState([])
+    const { slug } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (slug) {
+            service.getPosts(slug)
+                .then((post) => {
+                    if (post) {
+                        setPosts(post)
+                    }
+                })
+        } else {
+            navigate('/')
+        }
+    }, [slug, navigate])
+    return post ? (
+        <div className='py-8'>
+            <Container>
+                <PostForm {...post} />
+            </Container>
+        </div>
+    ) : null
+
+}
+
+export default EditPost
